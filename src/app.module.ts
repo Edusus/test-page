f0b75652b8dcd7a431d72382fa68554bec7392bf
Edusus <ejsucre.19@est.ucab.edu.ve>
2024-09-09 20:01:19 -0300

    connection with the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject:[ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: "localhost",
+        //configService.get('HOST'),
+        port: 5432,
+        //configService.get('PORT'),
+        username: "postgres",
+        //configService.get('USERNAME'),
+        password: "capibara123",
+        //configService.get('PASSWORD')
+        database: "postgres",
+        //configService.get('DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true
+      })
+    }),
+    UserModule,
+    ProductsModule,
+    OrdersModule,
+    AuthModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
